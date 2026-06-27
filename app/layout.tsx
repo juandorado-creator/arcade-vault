4315f4a6349d1cc938de30b995133346e2afd771
Juan Dorado <juan.dorado@qrvey.com>
2026-06-27 15:04:32 -0500

    paso 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Press_Start_2P, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Nav from "@/components/Nav";
 
 const pressStart2P = Press_Start_2P({
   weight: "400",
@@ -29,7 +30,10 @@ export default function RootLayout({
       lang="en"
       className={`${pressStart2P.variable} ${jetbrainsMono.variable} h-full`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+          <Nav />
+          {children}
+        </body>
     </html>
   );
 }
